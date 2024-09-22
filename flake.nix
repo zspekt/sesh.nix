ca0fb784332c6d0b89aed8fe1ec0424acbb1ace5
zspekt <zspekt@tutanota.com>
2024-09-22 09:14:36 -0300

    overlay

--- flake.nix
+++ flake.nix
@@ -7,20 +7,17 @@
 
   outputs =
     { self, nixpkgs }:
-    let
-      pkgs = import nixpkgs { system = "x86_64-linux"; };
-    in
     {
       packages = {
         # one entry for each arch
-        x86_64-linux = pkgs.callPackage ./default.nix { };
+        x86_64-linux = nixpkgs.lib.callPackage ./default.nix { };
+
+        aarch64-linux = nixpkgs.lib.callPackage ./default.nix { };
       };
 
-      # # overlay
-      # overlays = {
-      #   default = final: prev: {
-      #     customSesh = final.callPackage ./default.nix {};
-      #   };
-      # };
+      # overlay
+      overlays = {
+        default = final: prev: { customSesh = final.callPackage ./default.nix { }; };
+      };
     };
 }
