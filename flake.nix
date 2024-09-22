af1b54a384a4a6e7591a57782691be18726db9f2
zspekt <zspekt@tutanota.com>
2024-09-22 08:33:41 -0300

    flake
    Signed-off-by: zspekt <zspekt@tutanota.com>

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  description = "sesh pin commit";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs =
+    { self, nixpkgs }:
+    {
+      packages = {
+        # one entry for each arch
+        x86_64-linux = nixpkgs.lib.callPackage ./default.nix { };
+
+        aarch64-linux = nixpkgs.lib.callPackage ./default.nix { };
+      };
+
+      # # overlay
+      # overlays = {
+      #   default = final: prev: {
+      #     customSesh = final.callPackage ./default.nix {};
+      #   };
+      # };
+    };
+}
