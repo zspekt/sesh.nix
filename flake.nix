2f6034d6a4cbf60f2603793f3615538160c36717
zspekt <zspekt@tutanota.com>
2024-09-23 11:23:28 -0300

    test
    Signed-off-by: zspekt <zspekt@tutanota.com>

--- flake.nix
+++ flake.nix
@@ -1,27 +1,18 @@
 {
-  description = "sesh flake";
+  description = "sesh pinned commit flake";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
 
   outputs =
-    { nixpkgs, ... }:
-    let
-      # Import nixpkgs for different architectures
-      pkgs_x86_64 = import nixpkgs { system = "x86_64-linux"; };
-      pkgs_aarch64 = import nixpkgs { system = "aarch64-linux"; };
-    in
+    { self, nixpkgs }:
     {
-      packages = {
-        # Define packages for each architecture
-        x86_64-linux = pkgs_x86_64.lib.callPackage ./default.nix { };
-        aarch64-linux = pkgs_aarch64.lib.callPackage ./default.nix { };
-      };
-
-      # Define overlay for use in system configuration
-      overlays = {
-        default = final: prev: { customSesh = final.callPackage ./default.nix { }; };
-      };
+      defaultPackage.x86_64-linux =
+        (import nixpkgs { system = "x86_64-linux"; }).callPackage ./default.nix
+          { };
+      defaultPackage.aarch64-linux =
+        (import nixpkgs { system = "aarch64-linux"; }).callPackage ./default.nix
+          { };
     };
 }
