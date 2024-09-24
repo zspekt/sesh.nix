1f2657f26a534f24b24c47315eef68f8d7b1b857
zspekt <zspekt@tutanota.com>
2024-09-24 12:20:06 -0300

    final
    Signed-off-by: zspekt <zspekt@tutanota.com>

--- default.nix
+++ default.nix
@@ -8,7 +8,7 @@ buildGoModule rec {
     owner = "zspekt";
     repo = pname;
     rev = "23112682c82122e0fe0de0a38c7bd9a42e1c1444";
-    hash = "sha256-pd+BRgkbnCHLPeQDCxA3IlQl+rDbaQyPXJ+JiOAPt9U=";
+    hash = "sha256-vIHImJ9f//itNIG9TQY0c4l3DBAJ9heYV+EUy2KjmAw=";
   };
 
   vendorHash = "sha256-a45P6yt93l0CnL5mrOotQmE/1r0unjoToXqSJ+spimg=";
