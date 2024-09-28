ca91f920192c6f66ba3e8bc9aa0ae4734f65e92f
zspekt <zspekt@tutanota.com>
2024-09-27 23:32:34 -0300

    update
    Signed-off-by: zspekt <zspekt@tutanota.com>

--- default.nix
+++ default.nix
@@ -7,8 +7,8 @@ buildGoModule rec {
   src = fetchFromGitHub {
     owner = "zspekt";
     repo = pname;
-    rev = "ea5dc7da96dab8be726370c08a4252e7595b318d";
-    hash = "sha256-vIHImJ9f//itNIG9TQY0c4l3DBAJ9heYV+EUy2KjmAw=";
+    rev = "f6a5de5d51d92db439425966539bf61af5c23e59";
+    hash = "sha256-kKKJiCGfbj1ILdweoN/0yok8M8DfJwOeAGCf4TvSYfc=";
   };
 
   vendorHash = "sha256-a45P6yt93l0CnL5mrOotQmE/1r0unjoToXqSJ+spimg=";
